220fdb6b42ff668cb3e7e06d32da1084d31af245
Pál Szák <szak.pal@gmail.com>
2015-07-13 18:34:42 +0200

    travis test - release

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e 
+
+export GIT_COMMITTER_EMAIL=szak.pal@gmail.com
+export GIT_COMMITTER_NAME=PalSzak
+
+git checkout master
+git merge "$TRAVIS_COMMIT"
+
+git commit -m "Release with stable default head policy"
+
+git push --force "https://${GH_TOKEN}@${GH_REF}" master:master
