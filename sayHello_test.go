4c129206d6a9c6083daf410787818c2eb7821230
Pál Szák <szak.pal@gmail.com>
2015-07-13 17:35:15 +0200

    travis test6 - fix failing unit test

--- sayHello_test.go
+++ sayHello_test.go
@@ -4,5 +4,5 @@ import "testing"
 import "github.com/stretchr/testify/assert"
 
 func TestHello(t *testing.T){
-  assert.Equal(t, HelloWorld(), "Worldd", "name should be World");
+  assert.Equal(t, HelloWorld(), "World", "name should be World");
 }
