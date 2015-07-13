4f4349e2e5a4d5472e7acd0b8acb44cce26f2b76
Pál Szák <szak.pal@gmail.com>
2015-07-13 19:01:50 +0200

    travis test - fix git merge ?stragety?

--- release.sh
+++ release.sh
@@ -6,8 +6,4 @@ git config user.name "PalSzak"
 
 git checkout -b master
 
-git merge "$TRAVIS_COMMIT"
-
-git commit -m "Release with stable default head policy"
-
 git push --force "https://${GH_TOKEN}@${GH_REF}" master:master
